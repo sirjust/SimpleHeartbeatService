ed3b2f96502cd77bc9bfa95b1fd05dd7fe6ae309
Justin Moore <jmooretenor@gmail.com>
2019-09-11 20:27:22 -0400

    Initial commit

---
+++ SimpleHeartbeatService/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace SimpleHeartbeatService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<Heartbeat>(s =>
+                {
+                    s.ConstructUsing(heartbeat => new Heartbeat());
+                    s.WhenStarted(heartbeat => heartbeat.Start());
+                    s.WhenStopped(heartbeat => heartbeat.Stop());
+                });
+
+
+            });
+        }
+    }
+}
